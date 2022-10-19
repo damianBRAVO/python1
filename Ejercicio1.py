8e9f3634be83070c41f75e02c6b910f05c870fd7
DAMIAN BRAVO <102478250+damianBRAVO@users.noreply.github.com>
2022-10-19 11:15:10 -0300

    Initial commit

---
+++ Ejercicio1.py
@@ -0,0 +1,6 @@
+#EJERCICIO 1 OPERACION ARITMETICA
+a = float(input("a-> "))
+b = float(input("b-> "))
+c = float(input("c-> "))
+resultado = (a**3 * (b**2 - 2*a*c))/(2*b )
+print (f"El resultado es: {resultado}")
